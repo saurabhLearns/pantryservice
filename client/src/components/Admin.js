d82bdea2a8053608690a44c47c883e80fdfef75b
ShuRiknS <saurabh619@outlook.com>
2020-03-13 12:06:34 +0530

    tweaks

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 
+import {
+  Button, 
+  Dialog, 
+  ListItem, 
+  ListItemText, 
+  List, 
+  Divider, 
+  AppBar,
+  Toolbar, 
+  IconButton, 
+  Typography, 
+  Slide, 
+  Grid, 
+  Paper
+} from '@material-ui/core';
+
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,9 +9,9 @@ const auth = require('../../middleware/auth')
 const User = require('../../models/user')
 
 
-//Authenticate admin depending on frontend's switch bool value 'isadmin'
-router.post('/admin-login', function(req, res) {
-	const {email, password} = req.body
+//Authenticate user depending on frontend's switch bool value 'isadmin'
+router.post('/login', function(req, res) {
+	const {email, password, isAdmin} = req.body
 
 	if(!email || !password) 
 	return res.status(400).json({msg:"Please enter all fields!"})
@@ -19,7 +19,12 @@ router.post('/admin-login', function(req, res) {
 	User.findOne({email})
 	.then(user => {
 		if(!user)	return res.status(400).json({msg:"User does not exists!"})
-		if(user.role >= 3)	return res.status(400).json({msg:"You are not an admin!"})
+		if(isAdmin){
+			if(user.role >= 3)	return res.status(400).json({msg:"You are not an admin!"})
+		}
+		else if(user.role != 3){
+			return res.status(400).json({msg:"You are not a user!"})
+		}
 		bcrypt.compare(password, user.password)
 		.then(isMatch => {
 			if(!isMatch) 
@@ -46,41 +51,6 @@ router.post('/admin-login', function(req, res) {
 			)
 		})
 		.catch(err => res.status(400).json({msg:"unknown error"}))
-	}).catch(err => res.status(400).json({msg:"unknown error"}))
-})
-
-
-//authicate user after login and generate token
-router.post('/user-login', function(req, res) {
-	const {email, password} = req.body
-	if(!email || !password)	return res.status(400).json({msg:"Please enter all fields!"})
-	
-	User.findOne({email}).then(user => {
-		if(!user)	return res.status(400).json({msg:"User does not exists!"})
-		if(user.role != 3)	return res.status(400).json({msg:"You are not a user!"})
-		bcrypt.compare(password, user.password)
-		.then(isMatch => {
-			if(!isMatch)	return res.status(400).json({msg:"Invalid Credentials!"})
-			jwt.sign({	
-					id: user.id,
-					role: user.role
-				},
-				config.get('jwtSecret'),
-				{expiresIn: 10800},
-				(err, token) => {
-					if(err) throw err
-					res.json({
-						token,
-						user:{
-							id: user._id,
-							name: user.name,
-							email: user.email,
-							role: user.role
-						}
-					})
-				}
-			)
-		}).catch(err => res.status(400).json({msg:"unknown error"}))
 	}).catch(err => res.status(400).json({msg:"unknown error"}))
 })
 
